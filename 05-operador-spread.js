c9278aad49470aea773dbe7adddf8972c2bd322d
Jhonatan Vinicius <jhonnytrabalho2005@gmail.com>
2023-11-22 11:43:52 -0300

    Create 05-operador-spread.js

---
+++ 05-operador-spread.js
@@ -0,0 +1,9 @@
+/* ...spread-> espalhar
+Copiar elementos/dados de um array/objeto
+para dentro de outro array/objeto. */
+
+const bandas = ["The Strokes", "The Beatles", "Rush"];
+const maisBandas = [...bandas, "Slayer", "Nirvana"];
+
+console.log(bandas);
+console.log(maisBandas);
